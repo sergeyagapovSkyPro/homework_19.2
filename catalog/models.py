95ef83baeecef1297093d0d60336e5af630211bd
Segrey Agapov <stigrov@ya.ru>
2024-06-12 23:54:07 +0300

    Домашняя работа 23.1

--- catalog/models.py
+++ catalog/models.py
@@ -27,10 +27,16 @@ class Product(models.Model):
     created_at = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
     updated_at = models.DateTimeField(default=timezone.now, verbose_name='Дата последнего изменения')
     user = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name="Создан пользователем", **NULLABLE)
+    is_published = models.BooleanField(default=False, verbose_name='Признак публикации')
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+        permissions = [
+            ('set_published_status', 'Can publish product'),
+            ('change_description', 'Can change product description'),
+            ('change_category', 'Can change product category'),
+        ]
 
     def __str__(self):
         return f'{self.name} {self.price}'
